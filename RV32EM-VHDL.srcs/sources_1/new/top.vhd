b56e9edb9c99a8d3ba49ad6cc0fdde75526840b0
Francois-Xavier Rolland <rolland.fx@gmail.com>
2020-06-29 19:39:55 -0400

    very first pipeline idea

--- RV32EM-VHDL.srcs/sources_1/new/top.vhd
+++ RV32EM-VHDL.srcs/sources_1/new/top.vhd
@@ -11,17 +11,28 @@ i_reset : in STD_LOGIC
 end Main;
 
 architecture Behavioral of Main is
---signaux
+signal s_sig_1 : std_logic_vector(31 downto 0); -- signal de sortie pipeliné
+signal s_sig_2 : std_logic_vector(31 downto 0); -- signal de sortie pipeliné
+signal s_sig_3 : std_logic_vector(31 downto 0); -- signal de sortie pipeliné
+signal s_sig_4 : std_logic_vector(31 downto 0); -- signal de sortie pipeliné
+signal s_sig_5 : std_logic_vector(31 downto 0); -- signal de sortie pipeliné
 begin
 	MainControl:Process(i_clk) is -- processus synchrone
 		begin
 			if rising_edge(i_clk) then -- sur front montant d'horloge
 				if i_reset = '0' then -- reset synchrone =» remise de tout les registres à zéro
 					if i_run = '1' then
-					-- here when it runs
+					   s_sig_2 <= s_sig_1;
+					   s_sig_3 <= s_sig_2;
+					   s_sig_4 <= s_sig_3;
+					   s_sig_5 <= s_sig_4;	
 					end if; 
 				else -- reset =» remise a zéro de tout les registres
-				--reset stages
+				s_sig_1 <= (others => '0');
+				s_sig_2 <= (others => '0'); 
+				s_sig_3 <= (others => '0'); 
+				s_sig_4 <= (others => '0'); 
+				s_sig_5 <= (others => '0'); 		
 				end if;
 			end if;
 		end process;
