c9040a3b9faa53d32bcb3db970ee708ceb68d665
Francois-Xavier Rolland <rolland.fx@gmail.com>
2020-07-06 19:04:10 -0400

    generic mux of 32 bit input

---
+++ RV32EM-VHDL.srcs/sources_1/new/mux_type.vhd
@@ -0,0 +1,10 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+package mux_type is
+    type t_array is array (integer range <>) of std_logic_vector(31 downto 0);
+end package;
+
+package body mux_type is
+end package body;
