0ab3ada04e845b3c784e1af8be8e93ea34df43cc
FilipeAndres <filipe.andres.1@ens.etsmtl.ca>
2020-07-21 14:54:18 -0400

    Update control_unit.vhd

--- RV32EM-VHDL.srcs/sources_1/new/control_unit.vhd
+++ RV32EM-VHDL.srcs/sources_1/new/control_unit.vhd
@@ -11,8 +11,8 @@ entity control_unit is
 		IF_Flush   : out std_logic;
 		ID_Flush   : out std_logic;
 		EX_Flush   : out std_logic;
-		Control_WB : out std_logic_vector(1 downto 0); --
-		Control_M  : out std_logic_vector(4 downto 0); --
+		Control_WB : out std_logic_vector(1 downto 0); -- RegWrite(1) MemToReg(1) 
+		Control_M  : out std_logic_vector(4 downto 0); -- MemRead(1) MemWrite(1) MemRead(1) ??
 		Control_EX : out std_logic_vector(5 downto 0); -- ALUOp(2) ALUSrc1(2) ALUSrc2(2)
 		IS_Branch  : out std_logic;
 		IS_Jalr    : out std_logic
@@ -27,22 +27,27 @@ process(Opcode,Funct3)
 begin
 	case Opcode is
 		when "0110011" => --R type
-			if (Funct3 = ) then
-				
-			else
-
-			end if;
+			Exception  <= '0';
+			Jump       <= '0';
+			IF_Flush   <= '0';
+			ID_Flush   <= '0';
+			EX_Flush   <= '0';
+			Control_WB <= "10";
+			Control_M  <= "00000";
+			Control_EX <= "100000";
+			IS_Branch  <= '0';
+			IS_Jalr    <= '0';
 		when "0110111" => --LUI
-			Exception  <= ;
-			Jump       <= ;
-			IF_Flush   <= ;
-			ID_Flush   <= ;
-			EX_Flush   <= ;
-			Control_WB <= ;
-			Control_M  <= ;
-			Control_EX <= ;
-			IS_Branch  <= ;
-			IS_Jalr    <= ;
+			Exception  <= '0';
+			Jump       <= '0';
+			IF_Flush   <= '0';
+			ID_Flush   <= '0';
+			EX_Flush   <= '0';
+			Control_WB <= "10";
+			Control_M  <= "00000";
+			Control_EX <= "001001";
+			IS_Branch  <= '0';
+			IS_Jalr    <= '0';
 		when "0010111" => --AUIPC
 			Exception  <= '0';
 			Jump       <= '0';
@@ -138,11 +143,16 @@ begin
 				IS_Jalr    <= '0';
 			end if;
 		when "0010011" => --I type
-			if (Funct3 = ) then
-
-			else
-
-			end if;
+			Exception  <= '0';
+			Jump       <= '0';
+			IF_Flush   <= '0';
+			ID_Flush   <= '0';
+			EX_Flush   <= '0';
+			Control_WB <= "10";
+			Control_M  <= "00000";
+			Control_EX <= "100001";
+			IS_Branch  <= '0';
+			IS_Jalr    <= '0';
 		when "0000011" => --LOAD
 			if (Funct3 = "011" or Funct3 = "110" or Funct3 = "111") then
 				Exception  <= '1';
@@ -169,16 +179,16 @@ begin
 			end if;
 		when "0001111" => --FENCE
 			if (Funct3 = "000") then
-				Exception  <=;
-				Jump       <=;
-				IF_Flush   <=;
-				ID_Flush   <=;
-				EX_Flush   <=;
-				Control_WB <=;
-				Control_M  <=;
-				Control_EX <=;
-				IS_Branch  <=;
-				IS_Jalr    <=;
+				Exception  <= '0';
+				Jump       <= '0';
+				IF_Flush   <= '0';
+				ID_Flush   <= '0';
+				EX_Flush   <= '0';
+				Control_WB <= "00";
+				Control_M  <= "00000";
+				Control_EX <= "000000";
+				IS_Branch  <= '0';
+				IS_Jalr    <= '0';
 			else
 				Exception  <= '1';
 				Jump       <= '0';
@@ -191,7 +201,7 @@ begin
 				IS_Branch  <= '0';
 				IS_Jalr    <= '0';
 			end if;
-		when "1110011" => --ECALL
+		when "1110011" => --ECALL/EBREAK
 			if (Funct3 = "000") then
 				Exception  <= '0';
 				Jump       <= '0';
