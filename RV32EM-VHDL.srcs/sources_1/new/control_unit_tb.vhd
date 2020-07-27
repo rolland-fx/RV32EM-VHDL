d4a1710b800b868e01b3ece0a0dc8963da77f1f4
alexandreviau-ets <65986551+alexandreviau-ets@users.noreply.github.com>
2020-07-27 17:23:15 -0400

    control_unit_tb fix

--- RV32EM-VHDL.srcs/sources_1/new/control_unit_tb.vhd
+++ RV32EM-VHDL.srcs/sources_1/new/control_unit_tb.vhd
@@ -6,7 +6,7 @@
 -- Author      : User Name <user.email@user.company.com>
 -- Company     : User Company Name
 -- Created     : Mon Jul 27 15:41:25 2020
--- Last update : Mon Jul 27 16:08:34 2020
+-- Last update : Mon Jul 27 17:20:01 2020
 -- Platform    : Default Part Number
 -- Standard    : <VHDL-2008 | VHDL-2002 | VHDL-1993 | VHDL-1987>
 --------------------------------------------------------------------------------
@@ -22,6 +22,8 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+use std.textio.all;
+use ieee.std_logic_textio.all;
 
 library vunit_lib;
 context vunit_lib.vunit_context;
@@ -64,170 +66,170 @@ begin
 		Funct3 <= "001";
 		Opcode <= "1100111";
 		wait for 10 ns; --Invalid instruction
-		check_equal(Exception,std_logic('1'),"(Invalid instruction) Exception = 1");
-		check_equal(Jump,std_logic('0'),"(Invalid instruction) Jump = 0");
-		check_equal(IF_Flush,std_logic('1'),"(Invalid instruction) IF_Flush = 1");
-		check_equal(ID_Flush,std_logic('1'),"(Invalid instruction) ID_Flush = 1");
-		check_equal(EX_Flush,std_logic('1'),"(Invalid instruction) EX_Flush = 1");
-		check_equal(WB,std_logic_vector("00"),"(Invalid instruction) WB = 00");
-		check_equal(M,std_logic_vector("00"),"(Invalid instruction) M = 00");
-		check_equal(EX,std_logic_vector("000000"),"(Invalid instruction) EX = 000000");
-		check_equal(IS_Branch,std_logic('0'),"(Invalid instruction) IS_Branch = 0");
-		check_equal(IS_Jalr,std_logic('0'),"(Invalid instruction) IS_Jalr = 0");
+		check_equal(Exception,std_logic'('1'),"(Invalid instruction) Exception = 1");
+		check_equal(Jump,std_logic'('0'),"(Invalid instruction) Jump = 0");
+		check_equal(IF_Flush,std_logic'('1'),"(Invalid instruction) IF_Flush = 1");
+		check_equal(ID_Flush,std_logic'('1'),"(Invalid instruction) ID_Flush = 1");
+		check_equal(EX_Flush,std_logic'('1'),"(Invalid instruction) EX_Flush = 1");
+		check_equal(WB,std_logic_vector'("00"),"(Invalid instruction) WB = 00");
+		check_equal(M,std_logic_vector'("00"),"(Invalid instruction) M = 00");
+		check_equal(EX,std_logic_vector'("000000"),"(Invalid instruction) EX = 000000");
+		check_equal(IS_Branch,std_logic'('0'),"(Invalid instruction) IS_Branch = 0");
+		check_equal(IS_Jalr,std_logic'('0'),"(Invalid instruction) IS_Jalr = 0");
 
 		Funct3 <= "000";
 		Opcode <= "0110011";
 		wait for 10 ns; --R type (ADD)
-		check_equal(Exception,std_logic('0'),"(R type) Exception = 0");
-		check_equal(Jump,std_logic('0'),"(R type) Jump = 0");
-		check_equal(IF_Flush,std_logic('0'),"(R type) IF_Flush = 0");
-		check_equal(ID_Flush,std_logic('0'),"(R type) ID_Flush = 0");
-		check_equal(EX_Flush,std_logic('0'),"(R type) EX_Flush = 0");
-		check_equal(WB,std_logic_vector("10"),"(R type) WB = 10");
-		check_equal(M,std_logic_vector("00"),"(R type) M = 00");
-		check_equal(EX,std_logic_vector("100000"),"(R type) EX = 100000");
-		check_equal(IS_Branch,std_logic('0'),"(R type) IS_Branch = 0");
-		check_equal(IS_Jalr,std_logic('0'),"(R type) IS_Jalr = 0");
+		check_equal(Exception,std_logic'('0'),"(R type) Exception = 0");
+		check_equal(Jump,std_logic'('0'),"(R type) Jump = 0");
+		check_equal(IF_Flush,std_logic'('0'),"(R type) IF_Flush = 0");
+		check_equal(ID_Flush,std_logic'('0'),"(R type) ID_Flush = 0");
+		check_equal(EX_Flush,std_logic'('0'),"(R type) EX_Flush = 0");
+		check_equal(WB,std_logic_vector'("10"),"(R type) WB = 10");
+		check_equal(M,std_logic_vector'("00"),"(R type) M = 00");
+		check_equal(EX,std_logic_vector'("100000"),"(R type) EX = 100000");
+		check_equal(IS_Branch,std_logic'('0'),"(R type) IS_Branch = 0");
+		check_equal(IS_Jalr,std_logic'('0'),"(R type) IS_Jalr = 0");
 
 		Funct3 <= "000";
 		Opcode <= "1100011";
 		wait for 10 ns; --B type (BEQ)
-		check_equal(Exception,std_logic('0'),"(B type) Exception = 0");
-		check_equal(Jump,std_logic('1'),"(B type) Jump = 1");
-		check_equal(IF_Flush,std_logic('0'),"(B type) IF_Flush = 0");
-		check_equal(ID_Flush,std_logic('0'),"(B type) ID_Flush = 0");
-		check_equal(EX_Flush,std_logic('0'),"(B type) EX_Flush = 0");
-		check_equal(WB,std_logic_vector("00"),"(B type) WB = 00");
-		check_equal(M,std_logic_vector("00"),"(B type) M = 00");
-		check_equal(EX,std_logic_vector("010000"),"(B type) EX = 010000");
-		check_equal(IS_Branch,std_logic('1'),"(B type) IS_Branch = 1");
-		check_equal(IS_Jalr,std_logic('0'),"(B type) IS_Jalr = 0");
+		check_equal(Exception,std_logic'('0'),"(B type) Exception = 0");
+		check_equal(Jump,std_logic'('1'),"(B type) Jump = 1");
+		check_equal(IF_Flush,std_logic'('0'),"(B type) IF_Flush = 0");
+		check_equal(ID_Flush,std_logic'('0'),"(B type) ID_Flush = 0");
+		check_equal(EX_Flush,std_logic'('0'),"(B type) EX_Flush = 0");
+		check_equal(WB,std_logic_vector'("00"),"(B type) WB = 00");
+		check_equal(M,std_logic_vector'("00"),"(B type) M = 00");
+		check_equal(EX,std_logic_vector'("010000"),"(B type) EX = 010000");
+		check_equal(IS_Branch,std_logic'('1'),"(B type) IS_Branch = 1");
+		check_equal(IS_Jalr,std_logic'('0'),"(B type) IS_Jalr = 0");
 
 		Funct3 <= "010";
 		Opcode <= "0100011";
 		wait for 10 ns; --S type (SW)
-		check_equal(Exception,std_logic('0'),"(S type) Exception = 0");
-		check_equal(Jump,std_logic('0'),"(S type) Jump = 0");
-		check_equal(IF_Flush,std_logic('0'),"(S type) IF_Flush = 0");
-		check_equal(ID_Flush,std_logic('0'),"(S type) ID_Flush = 0");
-		check_equal(EX_Flush,std_logic('0'),"(S type) EX_Flush = 0");
-		check_equal(WB,std_logic_vector("01"),"(S type) WB = 01");
-		check_equal(M,std_logic_vector("01"),"(S type) M = 01");
-		check_equal(EX,std_logic_vector("000001"),"(S type) EX = 000001");
-		check_equal(IS_Branch,std_logic('0'),"(S type) IS_Branch = 0");
-		check_equal(IS_Jalr,std_logic('0'),"(S type) IS_Jalr = 0");
+		check_equal(Exception,std_logic'('0'),"(S type) Exception = 0");
+		check_equal(Jump,std_logic'('0'),"(S type) Jump = 0");
+		check_equal(IF_Flush,std_logic'('0'),"(S type) IF_Flush = 0");
+		check_equal(ID_Flush,std_logic'('0'),"(S type) ID_Flush = 0");
+		check_equal(EX_Flush,std_logic'('0'),"(S type) EX_Flush = 0");
+		check_equal(WB,std_logic_vector'("01"),"(S type) WB = 01");
+		check_equal(M,std_logic_vector'("01"),"(S type) M = 01");
+		check_equal(EX,std_logic_vector'("000001"),"(S type) EX = 000001");
+		check_equal(IS_Branch,std_logic'('0'),"(S type) IS_Branch = 0");
+		check_equal(IS_Jalr,std_logic'('0'),"(S type) IS_Jalr = 0");
 
 		Funct3 <= "010";
 		Opcode <= "0000011";
 		wait for 10 ns; -- Load (LW)
-		check_equal(Exception,std_logic('0'),"(Load) Exception = 0");
-		check_equal(Jump,std_logic('0'),"(Load) Jump = 0");
-		check_equal(IF_Flush,std_logic('0'),"(Load) IF_Flush = 0");
-		check_equal(ID_Flush,std_logic('0'),"(Load) ID_Flush = 0");
-		check_equal(EX_Flush,std_logic('0'),"(Load) EX_Flush = 0");
-		check_equal(WB,std_logic_vector("11"),"(Load) WB = 11");
-		check_equal(M,std_logic_vector("10"),"(Load) M = 10");
-		check_equal(EX,std_logic_vector("000001"),"(Load) EX = 000001");
-		check_equal(IS_Branch,std_logic('0'),"(Load) IS_Branch = 0");
-		check_equal(IS_Jalr,std_logic('0'),"(Load) IS_Jalr = 0");
+		check_equal(Exception,std_logic'('0'),"(Load) Exception = 0");
+		check_equal(Jump,std_logic'('0'),"(Load) Jump = 0");
+		check_equal(IF_Flush,std_logic'('0'),"(Load) IF_Flush = 0");
+		check_equal(ID_Flush,std_logic'('0'),"(Load) ID_Flush = 0");
+		check_equal(EX_Flush,std_logic'('0'),"(Load) EX_Flush = 0");
+		check_equal(WB,std_logic_vector'("11"),"(Load) WB = 11");
+		check_equal(M,std_logic_vector'("10"),"(Load) M = 10");
+		check_equal(EX,std_logic_vector'("000001"),"(Load) EX = 000001");
+		check_equal(IS_Branch,std_logic'('0'),"(Load) IS_Branch = 0");
+		check_equal(IS_Jalr,std_logic'('0'),"(Load) IS_Jalr = 0");
 
 		Funct3 <= "000";
 		Opcode <= "0010011";
 		wait for 10 ns; --I type (ADDI)
-		check_equal(Exception,std_logic('0'),"(I type) Exception = 0");
-		check_equal(Jump,std_logic('0'),"(I type) Jump = 0");
-		check_equal(IF_Flush,std_logic('0'),"(I type) IF_Flush = 0");
-		check_equal(ID_Flush,std_logic('0'),"(I type) ID_Flush = 0");
-		check_equal(EX_Flush,std_logic('0'),"(I type) EX_Flush = 0");
-		check_equal(WB,std_logic_vector("10"),"(I type) WB = 10");
-		check_equal(M,std_logic_vector("00"),"(I type) M = 00");
-		check_equal(EX,std_logic_vector("100001"),"(I type) EX = 100001");
-		check_equal(IS_Branch,std_logic('0'),"(I type) IS_Branch = 0");
-		check_equal(IS_Jalr,std_logic('0'),"(I type) IS_Jalr = 0");
+		check_equal(Exception,std_logic'('0'),"(I type) Exception = 0");
+		check_equal(Jump,std_logic'('0'),"(I type) Jump = 0");
+		check_equal(IF_Flush,std_logic'('0'),"(I type) IF_Flush = 0");
+		check_equal(ID_Flush,std_logic'('0'),"(I type) ID_Flush = 0");
+		check_equal(EX_Flush,std_logic'('0'),"(I type) EX_Flush = 0");
+		check_equal(WB,std_logic_vector'("10"),"(I type) WB = 10");
+		check_equal(M,std_logic_vector'("00"),"(I type) M = 00");
+		check_equal(EX,std_logic_vector'("100001"),"(I type) EX = 100001");
+		check_equal(IS_Branch,std_logic'('0'),"(I type) IS_Branch = 0");
+		check_equal(IS_Jalr,std_logic'('0'),"(I type) IS_Jalr = 0");
 
 		Funct3 <= "000";
 		Opcode <= "0110111";
 		wait for 10 ns; --LUI
-		check_equal(Exception,std_logic('0'),"(LUI) Exception = 0");
-		check_equal(Jump,std_logic('0'),"(LUI) Jump = 0");
-		check_equal(IF_Flush,std_logic('0'),"(LUI) IF_Flush = 0");
-		check_equal(ID_Flush,std_logic('0'),"(LUI) ID_Flush = 0");
-		check_equal(EX_Flush,std_logic('0'),"(LUI) EX_Flush = 0");
-		check_equal(WB,std_logic_vector("10"),"(LUI) WB = 10");
-		check_equal(M,std_logic_vector("00"),"(LUI) M = 00");
-		check_equal(EX,std_logic_vector("001001"),"(LUI) EX = 001001");
-		check_equal(IS_Branch,std_logic('0'),"(LUI) IS_Branch = 0");
-		check_equal(IS_Jalr,std_logic('0'),"(LUI) IS_Jalr = 0");
+		check_equal(Exception,std_logic'('0'),"(LUI) Exception = 0");
+		check_equal(Jump,std_logic'('0'),"(LUI) Jump = 0");
+		check_equal(IF_Flush,std_logic'('0'),"(LUI) IF_Flush = 0");
+		check_equal(ID_Flush,std_logic'('0'),"(LUI) ID_Flush = 0");
+		check_equal(EX_Flush,std_logic'('0'),"(LUI) EX_Flush = 0");
+		check_equal(WB,std_logic_vector'("10"),"(LUI) WB = 10");
+		check_equal(M,std_logic_vector'("00"),"(LUI) M = 00");
+		check_equal(EX,std_logic_vector'("001001"),"(LUI) EX = 001001");
+		check_equal(IS_Branch,std_logic'('0'),"(LUI) IS_Branch = 0");
+		check_equal(IS_Jalr,std_logic'('0'),"(LUI) IS_Jalr = 0");
 
 		Funct3 <= "000";
 		Opcode <= "0010111";
 		wait for 10 ns; --AUIPC
-		check_equal(Exception,std_logic('0'),"(AUIPC) Exception = 0");
-		check_equal(Jump,std_logic('0'),"(AUIPC) Jump = 0");
-		check_equal(IF_Flush,std_logic('0'),"(AUIPC) IF_Flush = 0");
-		check_equal(ID_Flush,std_logic('0'),"(AUIPC) ID_Flush = 0");
-		check_equal(EX_Flush,std_logic('0'),"(AUIPC) EX_Flush = 0");
-		check_equal(WB,std_logic_vector("10"),"(AUIPC) WB = 10");
-		check_equal(M,std_logic_vector("00"),"(AUIPC) M = 00");
-		check_equal(EX,std_logic_vector("000101"),"(AUIPC) EX = 000101");
-		check_equal(IS_Branch,std_logic('0'),"(AUIPC) IS_Branch = 0");
-		check_equal(IS_Jalr,std_logic('0'),"(AUIPC) IS_Jalr = 0");
+		check_equal(Exception,std_logic'('0'),"(AUIPC) Exception = 0");
+		check_equal(Jump,std_logic'('0'),"(AUIPC) Jump = 0");
+		check_equal(IF_Flush,std_logic'('0'),"(AUIPC) IF_Flush = 0");
+		check_equal(ID_Flush,std_logic'('0'),"(AUIPC) ID_Flush = 0");
+		check_equal(EX_Flush,std_logic'('0'),"(AUIPC) EX_Flush = 0");
+		check_equal(WB,std_logic_vector'("10"),"(AUIPC) WB = 10");
+		check_equal(M,std_logic_vector'("00"),"(AUIPC) M = 00");
+		check_equal(EX,std_logic_vector'("000101"),"(AUIPC) EX = 000101");
+		check_equal(IS_Branch,std_logic'('0'),"(AUIPC) IS_Branch = 0");
+		check_equal(IS_Jalr,std_logic'('0'),"(AUIPC) IS_Jalr = 0");
 
 		Funct3 <= "000";
 		Opcode <= "1101111";
 		wait for 10 ns; --JAL
-		check_equal(Exception,std_logic('0'),"(JAL) Exception = 0");
-		check_equal(Jump,std_logic('1'),"(JAL) Jump = 1");
-		check_equal(IF_Flush,std_logic('1'),"(JAL) IF_Flush = 1");
-		check_equal(ID_Flush,std_logic('0'),"(JAL) ID_Flush = 0");
-		check_equal(EX_Flush,std_logic('0'),"(JAL) EX_Flush = 0");
-		check_equal(WB,std_logic_vector("10"),"(JAL) WB = 10");
-		check_equal(M,std_logic_vector("00"),"(JAL) M = 00");
-		check_equal(EX,std_logic_vector("000110"),"(JAL) EX = 000110");
-		check_equal(IS_Branch,std_logic('0'),"(JAL) IS_Branch = 0");
-		check_equal(IS_Jalr,std_logic('0'),"(JAL) IS_Jalr = 0");
+		check_equal(Exception,std_logic'('0'),"(JAL) Exception = 0");
+		check_equal(Jump,std_logic'('1'),"(JAL) Jump = 1");
+		check_equal(IF_Flush,std_logic'('1'),"(JAL) IF_Flush = 1");
+		check_equal(ID_Flush,std_logic'('0'),"(JAL) ID_Flush = 0");
+		check_equal(EX_Flush,std_logic'('0'),"(JAL) EX_Flush = 0");
+		check_equal(WB,std_logic_vector'("10"),"(JAL) WB = 10");
+		check_equal(M,std_logic_vector'("00"),"(JAL) M = 00");
+		check_equal(EX,std_logic_vector'("000110"),"(JAL) EX = 000110");
+		check_equal(IS_Branch,std_logic'('0'),"(JAL) IS_Branch = 0");
+		check_equal(IS_Jalr,std_logic'('0'),"(JAL) IS_Jalr = 0");
 
 		Funct3 <= "000";
 		Opcode <= "1100111";
 		wait for 10 ns; --JALR
-		check_equal(Exception,std_logic('0'),"(JALR) Exception = 0");
-		check_equal(Jump,std_logic('1'),"(JALR) Jump = 1");
-		check_equal(IF_Flush,std_logic('1'),"(JALR) IF_Flush = 1");
-		check_equal(ID_Flush,std_logic('0'),"(JALR) ID_Flush = 0");
-		check_equal(EX_Flush,std_logic('0'),"(JALR) EX_Flush = 0");
-		check_equal(WB,std_logic_vector("10"),"(JALR) WB = 10");
-		check_equal(M,std_logic_vector("00"),"(JALR) M = 00");
-		check_equal(EX,std_logic_vector("000110"),"(JALR) EX = 000110");
-		check_equal(IS_Branch,std_logic('0'),"(JALR) IS_Branch = 0");
-		check_equal(IS_Jalr,std_logic('1'),"(JALR) IS_Jalr = 1");
+		check_equal(Exception,std_logic'('0'),"(JALR) Exception = 0");
+		check_equal(Jump,std_logic'('1'),"(JALR) Jump = 1");
+		check_equal(IF_Flush,std_logic'('1'),"(JALR) IF_Flush = 1");
+		check_equal(ID_Flush,std_logic'('0'),"(JALR) ID_Flush = 0");
+		check_equal(EX_Flush,std_logic'('0'),"(JALR) EX_Flush = 0");
+		check_equal(WB,std_logic_vector'("10"),"(JALR) WB = 10");
+		check_equal(M,std_logic_vector'("00"),"(JALR) M = 00");
+		check_equal(EX,std_logic_vector'("000110"),"(JALR) EX = 000110");
+		check_equal(IS_Branch,std_logic'('0'),"(JALR) IS_Branch = 0");
+		check_equal(IS_Jalr,std_logic'('1'),"(JALR) IS_Jalr = 1");
 
 		Funct3 <= "000";
 		Opcode <= "0001111";
 		wait for 10 ns; --FENCE
-		check_equal(Exception,std_logic('0'),"(FENCE) Exception = 0");
-		check_equal(Jump,std_logic('0'),"(FENCE) Jump = 0");
-		check_equal(IF_Flush,std_logic('0'),"(FENCE) IF_Flush = 0");
-		check_equal(ID_Flush,std_logic('0'),"(FENCE) ID_Flush = 0");
-		check_equal(EX_Flush,std_logic('0'),"(FENCE) EX_Flush = 0");
-		check_equal(WB,std_logic_vector("00"),"(FENCE) WB = 00");
-		check_equal(M,std_logic_vector("00"),"(FENCE) M = 00");
-		check_equal(EX,std_logic_vector("000000"),"(FENCE) EX = 000000");
-		check_equal(IS_Branch,std_logic('0'),"(FENCE) IS_Branch = 0");
-		check_equal(IS_Jalr,std_logic('0'),"(FENCE) IS_Jalr = 0");
+		check_equal(Exception,std_logic'('0'),"(FENCE) Exception = 0");
+		check_equal(Jump,std_logic'('0'),"(FENCE) Jump = 0");
+		check_equal(IF_Flush,std_logic'('0'),"(FENCE) IF_Flush = 0");
+		check_equal(ID_Flush,std_logic'('0'),"(FENCE) ID_Flush = 0");
+		check_equal(EX_Flush,std_logic'('0'),"(FENCE) EX_Flush = 0");
+		check_equal(WB,std_logic_vector'("00"),"(FENCE) WB = 00");
+		check_equal(M,std_logic_vector'("00"),"(FENCE) M = 00");
+		check_equal(EX,std_logic_vector'("000000"),"(FENCE) EX = 000000");
+		check_equal(IS_Branch,std_logic'('0'),"(FENCE) IS_Branch = 0");
+		check_equal(IS_Jalr,std_logic'('0'),"(FENCE) IS_Jalr = 0");
 
 		Funct3 <= "000";
 		Opcode <= "1110011";
 		wait for 10 ns; --ECALL
-		check_equal(Exception,std_logic('0'),"(ECALL) Exception = 0");
-		check_equal(Jump,std_logic('0'),"(ECALL) Jump = 0");
-		check_equal(IF_Flush,std_logic('0'),"(ECALL) IF_Flush = 0");
-		check_equal(ID_Flush,std_logic('0'),"(ECALL) ID_Flush = 0");
-		check_equal(EX_Flush,std_logic('0'),"(ECALL) EX_Flush = 0");
-		check_equal(WB,std_logic_vector("00"),"(ECALL) WB = 00");
-		check_equal(M,std_logic_vector("00"),"(ECALL) M = 00");
-		check_equal(EX,std_logic_vector("000000"),"(ECALL) EX = 000000");
-		check_equal(IS_Branch,std_logic('0'),"(ECALL) IS_Branch = 0");
-		check_equal(IS_Jalr,std_logic('0'),"(ECALL) IS_Jalr = 0");
+		check_equal(Exception,std_logic'('0'),"(ECALL) Exception = 0");
+		check_equal(Jump,std_logic'('0'),"(ECALL) Jump = 0");
+		check_equal(IF_Flush,std_logic'('0'),"(ECALL) IF_Flush = 0");
+		check_equal(ID_Flush,std_logic'('0'),"(ECALL) ID_Flush = 0");
+		check_equal(EX_Flush,std_logic'('0'),"(ECALL) EX_Flush = 0");
+		check_equal(WB,std_logic_vector'("00"),"(ECALL) WB = 00");
+		check_equal(M,std_logic_vector'("00"),"(ECALL) M = 00");
+		check_equal(EX,std_logic_vector'("000000"),"(ECALL) EX = 000000");
+		check_equal(IS_Branch,std_logic'('0'),"(ECALL) IS_Branch = 0");
+		check_equal(IS_Jalr,std_logic'('0'),"(ECALL) IS_Jalr = 0");
 
 		test_runner_cleanup(runner);
 	end process main;
